efcd2fc2f03fe314fc339c0276fae43766c58fa7
John Prince <jtprince@gmail.com>
2014-05-29 18:33:49 -0600

    finished last couple specs

--- spec/mspire/obo/group_spec.rb
+++ spec/mspire/obo/group_spec.rb
@@ -18,12 +18,30 @@ describe Mspire::Obo::Group do
       expect(subject.id_to_name!.id_to_name["IMS:1000013"]).to eq "unit"
     end
 
-    specify '#id_to_name! builds id_to_name meta hash' do
+    specify '#id_to_cast! builds id_to_name meta hash' do
       expect(subject.id_to_cast!.id_to_cast["UO:0000012"]).to eq false
       expect(subject.id_to_cast!.id_to_cast["IMS:1001207"]).to eq :to_f
     end
 
-    ## ... etc.
+    specify '#id_to_stanza! builds id_to_name meta hash' do
+      expect(subject.id_to_stanza!.id_to_stanza["UO:0000012"].class).to eq Obo::Stanza
+      expect(subject.id_to_stanza!.id_to_stanza["IMS:1001207"].class).to eq Obo::Stanza
+    end
+
+    specify '' do
+      expect(subject.name_to_id!.name_to_id['kelvin']).to eq "UO:0000012"
+      expect(subject.name_to_id!.name_to_id['unit']).to eq "UO:0000000"  # <- clobbered "IMS:1000013"
+    end
+
+    describe 'resolving collisions with name_to_id' do
+      specify '#name_to_id allows lookup with namespace' do
+        subject.name_to_id!
+        expect(subject.name_to_id.class).to eq Hash
+        expect(subject.name_to_id['unit']).to eq "UO:0000000"
+        expect(subject.name_to_id('unit')).to eq "UO:0000000"
+        expect(subject.name_to_id("unit", 'IMS')).to eq "IMS:1000013"
+      end
+    end
   end
 
   describe '#make_all! building all hashes' do
@@ -50,14 +68,5 @@ describe Mspire::Obo::Group do
     end
   end
 
-  describe 'resolving collisions with name_to_id' do
-    specify '#name_to_id allows lookup with namespace' do
-      subject.name_to_id!
-      expect(subject.name_to_id.class).to eq Hash
-      expect(subject.name_to_id['unit']).to eq "UO:0000000"
-      expect(subject.name_to_id('unit')).to eq "UO:0000000"
-      expect(subject.name_to_id("unit", 'IMS')).to eq "IMS:1000013"
-    end
-  end
-
+  
 end
